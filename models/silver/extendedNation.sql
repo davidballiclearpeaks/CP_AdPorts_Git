48f01dcd3e43a27a18097333a0e26af5128d7562
davidballiclearpeaks <david.balli@clearpeaks.com>
2025-07-31 14:39:16 +0200

    committed

--- models/silver/extendedNation.sql
+++ models/silver/extendedNation.sql
@@ -1,3 +1,3 @@
-select nationname, nationcomment, regionname, regioncomment
+select nationname, nationcomment, regionname, regioncomment,1 as dummy
 from {{ ref("stg_bronze__nation") }} n
 left join {{ ref("stg_bronze__region") }} r on r.r_regionkey = n.n_regionkey
